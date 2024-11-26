5237c53a676bb759a25a1d2c8f3cbf7cf4d07c8a
yuxxeun <itsyuxxeun@gmail.com>
2024-11-26 11:38:12 +0700

    cors enabled

--- app/index.js
+++ app/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require('cors');
 const { getStones, addStone, test, testQuery } = require("./stoneController");
+const corsOptions = {
+  origin: '*', // Allow only this origin
+  methods: 'GET',         // Allow only these HTTP methods
+  allowedHeaders: ['Content-Type'], // Allow only specific headers
+};
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // Routes
 app.get("/stone/:style", getStones);
